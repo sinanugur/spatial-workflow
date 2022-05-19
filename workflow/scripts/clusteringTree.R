0071b115f2224f08b37db31df2efe5f25d9c8ef0
Sinan U. Umu <sium@eduroam-193-157-140-204.wlan.uio.no>
2022-05-19 15:59:08 +0200

    marker plots

--- workflow/scripts/clusteringTree.R
+++ workflow/scripts/clusteringTree.R
@@ -14,4 +14,4 @@ Spatial_Data <- FindClusters(Spatial_Data, verbose = FALSE,resolution = seq(0.1,
 
 clustree(Spatial_Data) -> p1
 
-ggsave(paste0(sampleID,"/clusteringTree/clusteringTree-",sampleID,".pdf"),p1,width=8,height=15)
+ggsave(paste0("results/",sampleID,"/clusteringTree/clusteringTree-",sampleID,".pdf"),p1,width=8,height=15)
