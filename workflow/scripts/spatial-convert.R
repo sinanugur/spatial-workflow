7179e99d040c5ccf202262a0f51a8ee1e89d3920
Sinan Uğur Umu <sinanugur@gmail.com>
2022-06-16 15:34:23 +0200

    tangram initial

---
+++ workflow/scripts/spatial-convert.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+
+require(SeuratDisk)
+
+source("workflow/scripts/spatial-functions.R")
+arguments=commandArgs(TRUE)
+
+scrnaID=arguments[1]
+
+scrna_data=readRDS(paste0("scrna/",scrnaID,".rds"))
+
+
+DefaultAssay(scrna_data) <- "RNA"
+
+scrna_data@assays$SCT <- NULL
+
+
+SaveH5Seurat(scrna_data,paste0(scrnaID,".h5Seurat"))
+SeuratDisk::Convert(paste0(scrnaID,".h5Seurat"), dest = "h5ad")
+
+
+
+
+
+
---
+++ workflow/scripts/spatial-tangram.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import scanpy as sc
+import tangram as tg
+import pandas as pd
+import sys
+
+
+print(sys.argv[1])
+adata_st = sc.read_visium(path=sys.argv[1])
+adata_sc= sc.read(filename=sys.argv[2])
+
+adata_sc.X=adata_sc.raw.X.copy()
+
+
+tg.pp_adatas(adata_sc,adata_st,genes=None)
+
+ad_map = tg.map_cells_to_space(
+                   adata_sc, 
+                   adata_st,         
+                   mode='clusters',
+                   cluster_label='seurat_clusters')
+
+
+tg.project_cell_annotations(ad_map, adata_st, annotation="seurat_clusters")
+
+
+adata_st.obsm['tangram_ct_pred'].to_csv("test.txt")
