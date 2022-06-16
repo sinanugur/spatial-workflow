44ac2d9e79401363df4cd1dc5767fc98edab5832
Sinan Uğur Umu <sinanugur@gmail.com>
2022-06-16 15:41:32 +0200

    fixes

--- workflow/scripts/spatial-convert.R
+++ workflow/scripts/spatial-convert.R
@@ -16,6 +16,8 @@ DefaultAssay(scrna_data) <- "RNA"
 scrna_data@assays$SCT <- NULL
 
 
+UpdateSeuratObject(scrna_data) -> scrna_data
+
 SaveH5Seurat(scrna_data,paste0(scrnaID,".h5Seurat"))
 SeuratDisk::Convert(paste0(scrnaID,".h5Seurat"), dest = "h5ad")
 
