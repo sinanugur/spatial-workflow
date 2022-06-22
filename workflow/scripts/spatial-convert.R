e4722aff33fb723049029f7fc1c248db6ed41ec2
Sinan Uğur Umu <sinanugur@gmail.com>
2022-06-22 10:59:47 +0200

    factor convert to character for tangram

--- workflow/scripts/spatial-convert.R
+++ workflow/scripts/spatial-convert.R
@@ -16,7 +16,9 @@ scrna_data[["SCT"]] <- NULL
 
 DietSeurat(scrna_data) -> scrna_data
 
-AddMetaData(scrna_data,make.names(as.character(scrna_data$seurat_clusters)),"seurat_clusters_tangram") -> scrna_data
+scrna_data@meta.data %>% dplyr::mutate(dplyr::across(where(is.factor), as.character)) -> scrna_data@meta.data
+
+#AddMetaData(scrna_data,make.names(as.character(scrna_data$seurat_clusters)),"seurat_clusters_tangram") -> scrna_data
 
 
 SaveH5Seurat(scrna_data,paste0("scrna/",scrnaID,".h5Seurat"))
